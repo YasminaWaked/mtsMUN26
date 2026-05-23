6d5316d1f6ab69fd008c30ed61ec7fc58adc7ffb
yasminawaked <yasminaisswimming@gmail.com>
2026-05-23 11:27:15 +0400

    menu operational

--- mtsMUN26.tsx
+++ mtsMUN26.tsx
@@ -7,9 +7,10 @@ import StaggeredMenu from './menu';
 
 const menuItems = [
   { label: 'Home', ariaLabel: 'Go to home page', link: '/' },
-  { label: 'About', ariaLabel: 'Learn about us', link: '/about' },
-  { label: 'Services', ariaLabel: 'View our services', link: '/services' },
-  { label: 'Contact', ariaLabel: 'Get in touch', link: '/contact' }
+  { label: 'Register', ariaLabel: 'Register now!', link: '/register' },
+  { label: 'Committees', ariaLabel: 'Learn about our committees', link: '/committees' },
+  { label: 'Resources', ariaLabel: 'Resources for the conference', link: '/resources' },
+  { label: 'Gallery', ariaLabel: 'All of our photos!!', link: '/gallery' }
 ];
 
 const socialItems = [
